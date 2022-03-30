65ed1827f2bf7ab45fabf6bfbd12b05620e2adf4
David Amador <54488581+dsamador@users.noreply.github.com>
2022-03-30 13:23:23 -0500

    54 Interface Identity Definition

---
+++ CleanArchitecture.Application/Contracts/Identity/IAuthService.cs
@@ -0,0 +1,14 @@
+﻿using CleanArchitecture.Application.Models.Identity;
+
+namespace CleanArchitecture.Application.Contracts.Identity
+{
+    public interface IAuthService
+    {
+        //Definimos la tarea Login
+        Task<AuthResponse> Login(AuthRequest request);
+
+        //Definimos otra operacion para el registro de nuevos usuarios
+        Task<RegistrationResponse> Register(RegistrationRequest request);
+
+    }
+}
---
+++ CleanArchitecture.Application/Models/Identity/JwtSettings.cs
@@ -0,0 +1,12 @@
+﻿namespace CleanArchitecture.Application.Models.Identity
+{
+    public class JwtSettings
+    {
+        public string Key { get; set; } = string.Empty;
+        
+        // representa quien envia el token a los clientes
+        public string Issuer{ get; set; } = string.Empty;
+        public string Audience { get; set; } = string.Empty; //es la audiencia
+        public double DurationInMinutes { get; set; }
+    }
+}
