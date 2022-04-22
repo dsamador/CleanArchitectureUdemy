7bed3bdbe6e4f7e597323a4acf02d90849035443
David Amador <54488581+dsamador@users.noreply.github.com>
2022-04-22 14:27:20 -0500

    User personalized

---
+++ CleanArchitecture.Identity/CleanArchitectureIdentityDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace CleanArchitecture.Identity
+{
+    public class CleanArchitectureIdentityDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public CleanArchitectureIdentityDbContext(
+            DbContextOptions<CleanArchitectureIdentityDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            ModelBuilder.ApplyConfiguration(new RoleConfiguration());
+            ModelBuilder.ApplyConfiguration(new UserConfiguration());
+            ModelBuilder.ApplyConfiguration(new UserRoleConfiguration());
+        }
+    }
+}
