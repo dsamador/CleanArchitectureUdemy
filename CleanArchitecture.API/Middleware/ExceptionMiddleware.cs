dcdde189c95aff18a3646fed7941935a45288d7b
David Amador <54488581+dsamador@users.noreply.github.com>
2022-06-21 14:55:31 -0500

    Middleware tipado

--- CleanArchitecture.API/Middleware/ExceptionMiddleware.cs
+++ CleanArchitecture.API/Middleware/ExceptionMiddleware.cs
@@ -1,6 +1,7 @@
 ﻿using CleanArchitecture.API.Errors;
+using CleanArchitecture.Application.Exceptions;
 using System.Net;
-using System.Text.Json;
+using Newtonsoft.Json;
 
 namespace CleanArchitecture.API.Middleware
 {
@@ -16,7 +17,7 @@ namespace CleanArchitecture.API.Middleware
             _logger = logger;
             _env = env;
         }
-
+         
         public async Task InvokeAsync(HttpContext context)
         {
             try
@@ -27,17 +28,36 @@ namespace CleanArchitecture.API.Middleware
             {
                 _logger.LogError(ex, ex.Message);
                 context.Response.ContentType = "application/json";
-                context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
-                var response = _env.IsDevelopment()
-                    ? new CodeErrorsException
-                        ((int)HttpStatusCode.InternalServerError, ex.Message, ex.StackTrace)
-                    : new CodeErrorsException
-                        ((int)HttpStatusCode.InternalServerError);
                 
-                var options = new JsonSerializerOptions 
-                    { PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
-                var json = JsonSerializer.Serialize(response, options);
-                await context.Response.WriteAsync(json);
+                var statusCode = (int)HttpStatusCode.InternalServerError;
+                var result = string.Empty;
+
+                switch (ex)
+                {
+                    case NotFoundException notFoundException:
+                        statusCode = (int)HttpStatusCode.NotFound;
+                        break;
+
+                    case ValidationException validationException:
+                        statusCode = (int)HttpStatusCode.BadRequest;
+                        var validationJson = JsonConvert.SerializeObject(validationException.Errors);
+                        result = JsonConvert.SerializeObject(new CodeErrorsException(statusCode, ex.Message, validationJson));
+                        break;
+
+                    case BadRequestException badRequestException:
+                        statusCode = (int)HttpStatusCode.BadRequest;
+                        break;
+                    
+                    default:
+                        break;
+                }
+
+                if (string.IsNullOrEmpty(result))
+                    result = JsonConvert.SerializeObject(new CodeErrorsException(statusCode, ex.Message, ex.StackTrace));
+
+                context.Response.StatusCode = statusCode;
+
+                await context.Response.WriteAsync(result);
             }
         }
     }
