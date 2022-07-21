d1c0799cdb172ae919f33eb49917449110621c63
David Amador <54488581+dsamador@users.noreply.github.com>
2022-07-20 20:04:54 -0500

    Director con UnitOfWork
    Primero creamos las carpetas dentro de la capa Application, luego creamos el command, posteriormente el commandHandler.

---
+++ CleanArchitecture.Application/Features/Directors/Commands/CreateDirector/CreateDirectorCommand.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Application.Features.Directors.Commands.CreateDirector
+{
+    public class CreateDirectorCommand : IRequest<int>
+    {
+        public string? Nombre { get; set; } = string.Empty;
+        public string? Apellido { get; set; } = string.Empty;
+        public int VideoId { get; set; }
+    }
+}
---
+++ CleanArchitecture.Application/Features/Directors/Commands/CreateDirector/CreateDirectorCommandHandler.cs
@@ -0,0 +1,51 @@
+﻿using AutoMapper;
+using CleanArchitecture.Application.Contracts.Persistence;
+using CleanArchitecture.Domain;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Application.Features.Directors.Commands.CreateDirector
+{
+    public class CreateDirectorCommandHandler : IRequestHandler<CreateDirectorCommand, int>
+    {        
+
+        private readonly ILogger<CreateDirectorCommandHandler> _logger;
+        private readonly IMapper _mapper;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public CreateDirectorCommandHandler
+        (
+            ILogger<CreateDirectorCommandHandler> logger, 
+            IMapper mapper, 
+            IUnitOfWork unitOfWork
+        )
+        {
+            _logger = logger;
+            _mapper = mapper;
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<int> Handle(CreateDirectorCommand request,
+            CancellationToken cancellationToken)
+        {
+            var directorEntity = _mapper.Map<Director>(request);
+            _unitOfWork.Repository<Director>().AddEntity(directorEntity);
+            var result = await _unitOfWork.Complete();
+
+            if (result < 0) 
+            {
+                _logger.LogError("No se inserto el record del director");
+                throw new Exception("No se pudo insertar el record del director"); 
+            }
+
+            _logger.LogInformation($"Director {directorEntity.Id} fue creado exitosamente");
+            return directorEntity.Id;
+        }
+
+    }
+}
